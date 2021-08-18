b50c0088277ad0dc73f5f8421c5e382c6aea6a3c
“AlekSimpson” <alek@asu.me>
2021-08-17 21:18:12 -0700

    initial commit

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() 
+{
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello, World. 
+          <br/>I am a freelance web developer and programmer from Arizona. 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
